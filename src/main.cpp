8a9b4d9296a95713b50f2ef912f9818f4293ae08
amaan-on-compsci <amaankhanx16@gmail.com>
2023-10-07 17:38:49 -0700

    configured and built files

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
-#include<iostream>
-#include<SDL3/SDL.h>
+#include <iostream>
+#include <SDL3/SDL.h>
+#include <sndfile.hh>
+#include <stdio.h>
+#include "AudioFile.h"
 
-int main() {
-    
+int main()
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    {
+        std::cout << "Error : " << SDL_GetError() << std::endl;
+        return -1;
+    }
+    SDL_Window *window;
+    window = SDL_CreateWindow(
+        "my window",
+        320, 240, SDL_WINDOW_RESIZABLE);
+    int i = 0;
+    while (i < 100000)
+    {
+        i++;
+    }
+    SDL_Quit();
+    return 0;
 }
