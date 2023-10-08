239404319a31ce6337767268250ca46a5f6ed62b
amaan-on-compsci <amaankhanx16@gmail.com>
2023-10-07 20:26:01 -0700

    changed working environment to Windows from WSL2

--- src/main.cpp
+++ src/main.cpp
@@ -4,22 +4,45 @@
 #include <stdio.h>
 #include "AudioFile.h"
 
-int main()
+int main(int argc, char** argv)
 {
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
-    {
-        std::cout << "Error : " << SDL_GetError() << std::endl;
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        SDL_Log("SDL initialization failed: %s", SDL_GetError());
         return -1;
     }
-    SDL_Window *window;
-    window = SDL_CreateWindow(
-        "my window",
-        320, 240, SDL_WINDOW_RESIZABLE);
-    int i = 0;
-    while (i < 100000)
-    {
-        i++;
+    // Create a window
+    SDL_Window* window = SDL_CreateWindow(
+        "SDL Window",
+        800, // Width
+        600, // Height
+        SDL_WINDOW_RESIZABLE |
+        SDL_WINDOW_MAXIMIZED
+    );
+
+    if (!window) {
+        SDL_Log("Failed to create window: %s", SDL_GetError());
+        return -1;
     }
+
+    // Event loop
+    bool quit = false;
+    while (!quit) {
+        SDL_Event e;
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_EVENT_QUIT) {
+                quit = true;
+            }
+        }
+
+        // Render your content here
+        // SDL_RenderClear(renderer); // If you want to clear the window
+        // SDL_RenderPresent(renderer); // If you want to update the window
+    }
+
+    // Cleanup and quit
+    SDL_DestroyWindow(window);
     SDL_Quit();
+
     return 0;
 }
